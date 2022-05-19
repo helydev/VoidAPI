f949a492dcea67824023b12e37ffce35a3ee13ff
ByJoako <73551212+ByJoako@users.noreply.github.com>
2022-05-19 00:11:43 -0300

    Update VoidAPI.java

--- src/main/java/dev/hely/hcf/VoidAPI.java
+++ src/main/java/dev/hely/hcf/VoidAPI.java
@@ -14,112 +14,112 @@ public class VoidAPI {
     public static VoidAPI INSTANCE;
 
     public int getPlayerBalance(UUID uuid) {
-        throw new IllegalPluginAccessException("VoidAPI not found");
+        throw new IllegalPluginAccessException("VoidAPI was not found");
     }
 
     public int setPlayerBalance(UUID uuid, int balance) {
-        throw new IllegalPluginAccessException("VoidAPI not found");
+        throw new IllegalPluginAccessException("VoidAPI was not found");
     }
 
     public int addPlayerBalance(UUID uuid, int balance) {
-        throw new IllegalPluginAccessException("VoidAPI not found");
+        throw new IllegalPluginAccessException("VoidAPI was not found");
     }
 
     public int subtractPlayerBalance(UUID uuid, int balance) {
-        throw new IllegalPluginAccessException("VoidAPI not found");
+        throw new IllegalPluginAccessException("VoidAPI was not found");
     }
 
     public int getPlayerLives(UUID uuid) {
-        throw new IllegalPluginAccessException("VoidAPI not found");
+        throw new IllegalPluginAccessException("VoidAPI was not found");
     }
 
     public int setPlayerLives(UUID uuid, int balance) {
-        throw new IllegalPluginAccessException("VoidAPI not found");
+        throw new IllegalPluginAccessException("VoidAPI was not found");
     }
 
     public int addPlayerLives(UUID uuid, int balance) {
-        throw new IllegalPluginAccessException("VoidAPI not found");
+        throw new IllegalPluginAccessException("VoidAPI was not found");
     }
 
     public int subtractPlayerLives(UUID uuid, int balance) {
-        throw new IllegalPluginAccessException("VoidAPI not found");
+        throw new IllegalPluginAccessException("VoidAPI was not found");
     }
 
     public int getKills(UUID uuid) {
-        throw new IllegalPluginAccessException("VoidAPI not found");
+        throw new IllegalPluginAccessException("VoidAPI was not found");
     }
 
     public void setKills(UUID uuid, int kills) {
-        throw new IllegalPluginAccessException("VoidAPI not found");
+        throw new IllegalPluginAccessException("VoidAPI was not found");
     }
 
     public void addKills(UUID uuid, int kills) {
-        throw new IllegalPluginAccessException("VoidAPI not found");
+        throw new IllegalPluginAccessException("VoidAPI was not found");
     }
     public int getDeaths(UUID uuid) {
-        throw new IllegalPluginAccessException("VoidAPI not found");
+        throw new IllegalPluginAccessException("VoidAPI was not found");
     }
 
     public void setDeaths(UUID uuid, int deaths) {
-        throw new IllegalPluginAccessException("VoidAPI not found");
+        throw new IllegalPluginAccessException("VoidAPI was not found");
     }
 
     public void addDeaths(UUID uuid, int kills) {
-        throw new IllegalPluginAccessException("VoidAPI not found");
+        throw new IllegalPluginAccessException("VoidAPI was not found");
     }
 
     public int getStreak(UUID uuid) {
-        throw new IllegalPluginAccessException("VoidAPI not found");
+        throw new IllegalPluginAccessException("VoidAPI was not found");
     }
     public void setStreak(UUID uuid, int streak) {
-        throw new IllegalPluginAccessException("VoidAPI not found");
+        throw new IllegalPluginAccessException("VoidAPI was not found");
     }
 
     public void addStreak(UUID uuid, int streak) {
-        throw new IllegalPluginAccessException("VoidAPI not found");
+        throw new IllegalPluginAccessException("VoidAPI was not found");
     }
 
     public void removeCredits(UUID uuid, int credits) {
-        throw new IllegalPluginAccessException("VoidAPI not found");
+        throw new IllegalPluginAccessException("VoidAPI was not found");
     }
 
     public void addCredits(UUID uuid, int credits) {
-        throw new IllegalPluginAccessException("VoidAPI not found");
+        throw new IllegalPluginAccessException("VoidAPI was not found");
     }
 
     public void setHighstreak(UUID uuid, int amounts) {
-        throw new IllegalPluginAccessException("VoidAPI not found");
+        throw new IllegalPluginAccessException("VoidAPI was not found");
     }
 
     public boolean isStaffMode(Player player){
-        throw new IllegalPluginAccessException("VoidAPI not found");
+        throw new IllegalPluginAccessException("VoidAPI was not found");
     }
 
     public boolean isStaffBuild(Player player){
-        throw new IllegalPluginAccessException("VoidAPI not found");
+        throw new IllegalPluginAccessException("VoidAPI was not found");
     }
 
     public boolean isStaffBoard(Player player){
-        throw new IllegalPluginAccessException("VoidAPI not found");
+        throw new IllegalPluginAccessException("VoidAPI was not found");
     }
 
     public boolean isVanish(Player player){
-        throw new IllegalPluginAccessException("VoidAPI not found");
+        throw new IllegalPluginAccessException("VoidAPI was not found");
     }
 
     public void giveStaffMode(Player player) {
-        throw new IllegalPluginAccessException("VoidAPI not found");
+        throw new IllegalPluginAccessException("VoidAPI was not found");
     }
 
     public void removeStaffMode(Player player) {
-        throw new IllegalPluginAccessException("VoidAPI not found");
+        throw new IllegalPluginAccessException("VoidAPI was not found");
     }
 
     public void giveVanish(Player player) {
-        throw new IllegalPluginAccessException("VoidAPI not found");
+        throw new IllegalPluginAccessException("VoidAPI was not found");
     }
 
     public void removeVanish(Player player) {
-        throw new IllegalPluginAccessException("VoidAPI not found");
+        throw new IllegalPluginAccessException("VoidAPI was not found");
     }
 }
